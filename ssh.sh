65a37fe2632ada981ec0b3d437246d2ed7c90a68
Vadim <shiva173@rambler.ru>
2021-08-20 10:08:09 +0300

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
-<<<<<<< HEAD
-servers_file=servers
-for var in $(cat $servers_file)
-=======
 serversfile=servers
 for var in $(cat $serversfile)
->>>>>>> 485c1a314ab4a5ed8b2bf22527a6e2946ddafb47
 do
 ssh-copy-id $var
 done
